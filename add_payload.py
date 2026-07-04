9d64adc3ef7e88e15994f5fc6a8a0cc1a0134b4e
speedihb2k15 <2sure3@gmail.com>
2026-07-04 19:08:45 +0200

    update

--- add_payload.py
+++ add_payload.py
@@ -13,7 +13,7 @@ from update_payloads import update_readme
 
 JSON_FILE = "payloads.json"
 PAYLOADS_DIR = "payloads"
-BASE_URL = "https://github.com/itsPLK/ps5-payloads-mirror/releases/download/payloads-mirror"
+BASE_URL = "https://github.com/speedihb2k15/speedi.github.io/releases/tag/Payloads"
 
 def get_repo_info(url):
     match = re.search(r"https?://([^/]+)/([^/]+)/([^/]+)", url)
